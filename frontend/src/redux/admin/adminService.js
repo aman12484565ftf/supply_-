037165fa8c37bb4aab3dbd43f5b9c3936260e3b3
aMan404deep <amandeep101003@gmail.com>
2025-03-31 12:45:15 +0530

    added dist folder

--- frontend/src/redux/admin/adminService.js
+++ frontend/src/redux/admin/adminService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/admin"; // Ensure correct base URL
+const API_URL = "https://supplychainpro.onrender.com/api/admin"; // Ensure correct base URL
 
 export const getDashboardStats = async (token) => {
   if (!token) {
